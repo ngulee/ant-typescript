3ff3a312e189f14ddecc6ec2fda9d32a6f090bc8
lizuoxing <lizuoxing4534@gmail.com>
2020-09-21 18:31:03 +0800

    products

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
+import { AppProvider } from './components/ContextPage/context';
+
 import ContextPage from './components/ContextPage';
 import ComponentPage from './components/ComponentPage';
+import Products from './components/ContextPage/_components/Products';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route exact path="/context" component={ContextPage} />
-          <Route exact path="/component_page" component={ComponentPage} />
-        </Switch>
-      </div>
+    <AppProvider>
+      <BrowserRouter>
+        <div className="App">
+          <Switch>
+            <Route exact path="/context" component={Products} />
+            <Route exact path="/component_page" component={ComponentPage} />
+          </Switch>
+        </div>
     </BrowserRouter>
+    </AppProvider>
+    
   );
 }
 
